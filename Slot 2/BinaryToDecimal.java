083d0874c05f47acd48cb48c134c248a49078d3a
Devikakhunt9 <104673812+Devikakhunt9@users.noreply.github.com>
2024-06-08 09:09:09 +0530

    slot 2 com

--- Slot 2/BinaryToDecimal.java
+++ Slot 2/BinaryToDecimal.java
@@ -10,36 +10,33 @@ public class BinaryToDecimal {
 
     public static  void convertBineryToDecimal(String b){
         double sum = 0;int index = b.length();
-     
+        String fraction = "";
             index = b.indexOf('.');
-           
-            index= index==-1?b.length():index;
-            //  System.out.println(index);
+           if(index!=-1){
+             fraction = b.substring(index+1,b.length());
+           }
+           else{
+            index = b.length();
+           }          
         String decimal = (b.substring(0,index));
-        String fraction = b.substring(index+1,b.length());
-        // System.out.println(decimal + "" + fraction);
+       
+        
             int temp = index-1;
-            // System.out.println(temp);
             char[] dcarray = decimal.toCharArray();
             char[] fractionArray = fraction.toCharArray();
         for (int i = 0; i < decimal.length(); i++) {
 
-            // System.out.println("Temp:"+temp);
             double pow = Math.pow(2, temp);
             temp--;
-            // System.out.println("power:"+pow);
             if(dcarray[i]=='1'){
                 sum = sum + pow * 1;
             }
             
         }
-        // System.out.println("sum : "+sum); 
         for (int i = 0; i < fraction.length(); i++) {
 
-            // System.out.println("Temp:"+temp);
             double pow = Math.pow(2, temp);
             temp--;
-            // System.out.println("power:"+pow);
             if(fractionArray[i]=='1'){
                 sum = sum + pow * 1;
             }
